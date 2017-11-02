cefc6b411c1e22f9105f574e4b1fe112221af1fb
joaoramia <joao.ramia@gmail.com>
2017-11-02 16:54:47 -0200

    fixed express server

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-var express = require('express');
-var app = express();
-var path = require('path');
 
-app.use(express.static(path.join(__dirname + '/dist')));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-var server = app.listen(Number(process.env.PORT) || 8080, function () {
-  console.log('App listening on port 8080!');
-});
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
