e5f2881dd1b571bc265d9e9305084d80d29b81db
joaoramia <joao.ramia@gmail.com>
2017-11-02 16:06:34 -0200

    including font awesome cdn for deploy;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-// fontawesome for the icons in the notification module
-// it has also been included in the angular-cli.json file inside 'styles'
-import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
-
 import { AppComponent } from './app.component';
 import { NotificationComponent } from './notification/notification.component';
 import { MockNotificationsComponent } from './mock-notifications/mock-notifications.component';
@@ -20,8 +16,7 @@ import { NotificationService } from './notification/notification.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AngularFontAwesomeModule
+    FormsModule
   ],
   providers: [
     // added here so it can be used by both the NotificationComponent and MockNotificationsComponent
